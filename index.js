e490f6a3dc312c8cfb197c59f088092817860128
darshan9405 <narendarbhandari9188@gmail.com>
2022-04-07 16:41:49 +0530

    Added login Page

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 4000
+app.use('/public', express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.listen(port,()=>{
+    console.log(`Server running successfully...\nLink for the server:-http://127.0.0.1:${port}`);
+})
