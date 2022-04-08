0aa2f8d1566791f49614860b141205505922339a
darshan9405 <narendarbhandari9188@gmail.com>
2022-04-08 20:58:47 +0530

    modify category page

--- public/js/category.js
+++ public/js/category.js
@@ -1,62 +1,63 @@
-const categories = [{
-        title: "Editor's Picks",
-        img: 'public/images/Editor.jpg',
-        hreflink: '/editor-picks'
-    },
-    {
-        title: 'Most Popular',
-        img: 'public/images/most.jpg',
-        hreflink: '/most-popular'
-    },
-    {
-        title: 'Academic & Education',
-        img: 'public/images/academic.jpg',
-        hreflink: '/Academic&Education'
-    },
-    {
-        title: 'Art',
-        img: 'public/images/art.jpg',
-        hreflink: '/art'
-    },
-    {
-        title: 'Biography',
-        img: 'public/images/biography.jpg',
-        hreflink: 'biography'
-    },
-    {
-        title: 'Business & Career',
-        img: 'public/images/business.jpg',
-        hreflink: 'Business&Career'
-    },
-    {
-        title: 'Environment',
-        img: 'public/images/env.jpg',
-        hreflink: 'Environment'
-    },
-    {
-        title: 'Health & Fitness',
-        img: 'public/images/health.jpg',
-        hreflink: 'Health&Fitness'
-    },
-    {
-        title: 'Technology',
-        img: 'public/images/tech.jpg',
-        hreflink: 'Technology'
-    },
-    {
-        title: 'Science & Research',
-        img: 'public/images/science.jpg',
-        hreflink: 'Science&Research'
-    }
+const categories = [
+  {
+    title: "Editor's Picks",
+    img: 'images/Editor.jpg',
+    hreflink: '/editor-picks'
+  },
+  {
+    title: 'Most Popular',
+    img: 'images/most.jpg',
+    hreflink: '/most-popular'
+  },
+  {
+    title: 'Academic & Education',
+    img: 'images/academic.jpg',
+    hreflink: '/Academic&Education'
+  },
+  {
+    title: 'Art',
+    img: 'images/art.jpg',
+    hreflink: '/art'
+  },
+  {
+    title: 'Biography',
+    img: 'images/biography.jpg',
+    hreflink: 'biography'
+  },
+  {
+    title: 'Business & Career',
+    img: 'images/business.jpg',
+    hreflink: 'Business&Career'
+  },
+  {
+    title: 'Environment',
+    img: 'images/env.jpg',
+    hreflink: 'Environment'
+  },
+  {
+    title: 'Health & Fitness',
+    img: 'images/health.jpg',
+    hreflink: 'Health&Fitness'
+  },
+  {
+    title: 'Technology',
+    img: 'images/tech.jpg',
+    hreflink: 'Technology'
+  },
+  {
+    title: 'Science & Research',
+    img: 'images/science.jpg',
+    hreflink: 'Science&Research'
+  }
+  ,
+  {
+    title: 'Personal Growth',
+    img: 'images/personal.jpg',
+    hreflink: 'personal-growth'
+  },
+  {
+    title: 'Fiction & Literature',
+    img: 'images/fic.jpg',
+    hreflink: 'Fiction&Literature'
+  }
 ]
-document.getElementById('main_container').innerHTML = ''
-categories.map(element => {
-    document.getElementById('main_container').innerHTML += `
-  <a class="card" style="width: 10rem" href=${element.hreflink}>
-  <img src=${element.img} class="card-img-top" alt="..." />
-  <div class="card-body">
-    <h5 class="card-title">${element.title}</h5>
-  </div>
-</a>
-`
-})
