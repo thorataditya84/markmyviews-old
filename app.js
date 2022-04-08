7a7d213458c28563572ebf00f91af5db3ab87cc0
darshan9405 <narendarbhandari9188@gmail.com>
2022-04-08 20:04:11 +0530

    Merge changes

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
 
-const app = express();
+const app = express()
 
-const port = 4000;
+const port = 4000
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+)
 
-app.use(express.static('public'));
-
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home')
 })
 
 app.get('/login', (req, res) => {
     res.render('login')
 })
 
+app.get('/category', (req, res) => {
+    res.render('category')
+})
+
 app.listen(port, () => {
-    console.log(`Server running successfully...\nLink for the server:-http://127.0.0.1:${port}`);
+    console.log(
+        `Server running successfully...\nLink for the server:-http://127.0.0.1:${port}`
+    )
 })
