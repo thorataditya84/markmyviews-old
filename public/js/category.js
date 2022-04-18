986e7a1ed446206eb3144f326a693c5e450cbdac
darshan9405 <narendarbhandari9188@gmail.com>
2022-04-18 17:59:22 +0530

    add changes here

---
+++ public/js/category.js
@@ -0,0 +1,62 @@
+const categories = [
+  {
+    title: "Editor's Picks",
+    img: 'images/Editor.jpg',
+    hreflink: '/books:editor-picks'
+  },
+  {
+    title: 'Most Popular',
+    img: 'images/most.jpg',
+    hreflink: '/books:most-popular'
+  },
+  {
+    title: 'Academic & Education',
+    img: 'images/academic.jpg',
+    hreflink: '/books:Academic&Education'
+  },
+  {
+    title: 'Art',
+    img: 'images/art.jpg',
+    hreflink: '/books:art'
+  },
+  {
+    title: 'Biography',
+    img: 'images/biography.jpg',
+    hreflink: '/books:biography'
+  },
+  {
+    title: 'Business & Career',
+    img: 'images/business.jpg',
+    hreflink: '/books:Business&Career'
+  },
+  {
+    title: 'Environment',
+    img: 'images/env.jpg',
+    hreflink: '/books:Environment'
+  },
+  {
+    title: 'Health & Fitness',
+    img: 'images/health.jpg',
+    hreflink: '/books:Health&Fitness'
+  },
+  {
+    title: 'Technology',
+    img: 'images/tech.jpg',
+    hreflink: '/books:Technology'
+  },
+  {
+    title: 'Science & Research',
+    img: 'images/science.jpg',
+    hreflink: '/books:Science&Research'
+  },
+  {
+    title: 'Personal Growth',
+    img: 'images/personal.jpg',
+    hreflink: '/books:personal-growth'
+  },
+  {
+    title: 'Fiction & Literature',
+    img: 'images/fic.jpg',
+    hreflink: '/books:Fiction&Literature'
+  }
+]
