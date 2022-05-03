3d90d054829ebe0aca75012c3c13ccce4b30f490
darshan9405 <narendarbhandari9188@gmail.com>
2022-05-03 10:26:46 +0530

    responsive changes

--- public/js/home.js
+++ public/js/home.js
@@ -102,3 +102,20 @@ function signUpHandler (event) {
 //validations for login page;
 
 //validations for SignUp page;
+//loader javascript;
+function onReady (callback) {
+  var intervalId = window.setInterval(function () {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId)
+      callback.call(this)
+    }
+  }, 1000)
+}
+
+function setVisible (selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none'
+}
+
+onReady(function () {
+  setVisible('#loading', false)
+})
